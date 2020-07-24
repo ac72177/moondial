30f76ccb664a10e78e8eec30021de1ce8592a389
andreasy <ac72177@yahoo.com>
2020-07-24 16:33:09 -0700

    continuing to design tests and implementation of EntryList

--- src/main/model/EntryList.java
+++ src/main/model/EntryList.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 // Represents a list of observations of the moon
 public class EntryList {
-    protected final List<Entry> entries;
+    private final List<Entry> entries;
+    private final List<Integer> sortedByPhase = null;
 
     // EFFECTS: entries is empty
     public EntryList() {
@@ -37,6 +38,41 @@ public class EntryList {
     public int size() {
         return entries.size();
     }
+
+    // EFFECTS: creates an empty list
+    private SortedListByPhase() {
+        sortedByPhase = new ArrayList<>();
+    }
+    public List<Integer> sortAndCountListByPhase(EntryList entries) {
+        for (Entry item : entries) {
+            if (item.getMoonPhase().equals("New Moon")) {
+                newM = newM + 1;
+                sortedByPhase.add(0, newM);
+            } else if (item.getMoonPhase().equals("Waxing Crescent")) {
+                waxCr = waxCr + 1;
+                sortedByPhase.add(1, waxCr);
+            } else if (item.getMoonPhase().equals("First Quarter")) {
+                firstQ = firstQ + 1;
+                sortedByPhase.add(2, firstQ);
+            } else if (item.getMoonPhase().equals("Waxing Gibbous")) {
+                waxGib = waxGib + 1;
+                sortedByPhase.add(3, waxGib);
+            } else if (item.getMoonPhase().equals("Full Moon")) {
+                fullM = fullM + 1;
+                sortedByPhase.add(4, fullM);
+            } else if (item.getMoonPhase().equals("Waning Gibbous")) {
+                wanGib = wanGib + 1;
+                sortedByPhase.add(5, wanGib);
+            } else if (item.getMoonPhase().equals("Third Quarter")) {
+                thirdQ = thirdQ + 1;
+                sortedByPhase.add(6, thirdQ);
+            } else {
+                wanCr = wanCr + 1;
+                sortedByPhase.add(7, wanCr);
+            }
+}
+        return sortedByPhase;
+                }
 }
 
 
